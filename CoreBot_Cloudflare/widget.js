20736eaef40c9d2301cb00471c591d291a855910
coreposturebot <tmeierdc@gmail.com>
2025-07-17 22:18:50 -0700

    Add files via upload

---
+++ CoreBot_Cloudflare/widget.js
@@ -0,0 +1,6 @@
+
+// Simulated chatbot widget script
+document.addEventListener("DOMContentLoaded", function () {
+  const widget = document.getElementById("corebot-widget");
+  widget.innerHTML = "<p>[CoreBot is ready to chat]</p>";
+});
