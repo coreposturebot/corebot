4c8bbeaf081582e38ce34149190914cca68ffd59
coreposturebot <tmeierdc@gmail.com>
2025-08-02 07:15:03 -0700

    Delete CoreBot_Cloudflare directory

--- CoreBot_Cloudflare/widget.js
+++
@@ -1,6 +0,0 @@
-
-// Simulated chatbot widget script
-document.addEventListener("DOMContentLoaded", function () {
-  const widget = document.getElementById("corebot-widget");
-  widget.innerHTML = "<p>[CoreBot is ready to chat]</p>";
-});
